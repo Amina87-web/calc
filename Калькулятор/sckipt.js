292e590d5b5feca05211d38a0ba19ee6cb943a97
Amina87-wed <vvv19232@gmail.com>
2024-10-22 01:14:08 +0300

    first commit

---
+++ Калькулятор/sckipt.js
@@ -0,0 +1,89 @@
+rez_btn.onclick = function(){
+   rez.value =eval(rez.value);
+
+}
+
+let r = 0;
+
+in_1.onclick = function() {
+    r = rez.value;
+    rez.value=r+1;
+}
+in_2.onclick = function() {
+    r = rez.value;
+    rez.value=r+2;}
+
+ in_2.onclick = function() {
+        r = rez.value;
+        rez.value=r+2;}
+
+in_3.onclick = function() {
+    r = rez.value;
+    rez.value=r+3;}
+
+ in_4.onclick = function() {
+        r = rez.value;
+        rez.value=r+4;}  
+
+ in_5.onclick = function() {
+            r = rez.value;
+            rez.value=r+5;}   
+            
+ in_6.onclick = function() {
+         r = rez.value;
+    rez.value=r+6;}
+
+in_7.onclick = function() {
+        r = rez.value;
+        rez.value=r+7;}
+
+in_8.onclick = function() {
+    r = rez.value;
+     rez.value=r+8;} 
+
+            
+in_9.onclick = function() {
+     r = rez.value;
+ rez.value=r+9;}
+
+in_0.onclick = function() {
+    r = rez.value;
+    rez.value=r+0;}
+
+in_th.onclick = function() {
+        r = rez.value;
+        rez.value=r+'.';}  
+     
+       
+in_plus.onclick = function() {
+     r = rez.value;
+     rez.value=r+'+';}
+
+in_minus.onclick = function() {
+    r = rez.value;
+    rez.value=r+'-';}
+
+in_x.onclick = function() {
+       r = rez.value;
+     rez.value=r+'*';}  
+
+ in_del.onclick = function() {
+        r = rez.value;
+        rez.value=r+'/';}  
+
+ in_rez_btn.onclick = function() {
+     r = rez.value;
+    rez.value=r+'=';}  
+
+ in_th.onclick = function() {
+     r = rez.value;
+     rez.value=r+'.';}  
+    
+
+    
+     
+     
+  
+        
+     
+                
